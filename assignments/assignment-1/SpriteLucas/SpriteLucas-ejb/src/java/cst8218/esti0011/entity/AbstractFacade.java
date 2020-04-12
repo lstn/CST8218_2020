25c8eed4325900d07481e1ccf072c17ca94c043a
Lucas Estienne <lucas.estienne@cira.ca>
2020-04-11 22:33:10 -0400

    restore assign 1

---
+++ assignments/assignment-1/SpriteLucas/SpriteLucas-ejb/src/java/cst8218/esti0011/entity/AbstractFacade.java
@@ -0,0 +1,65 @@
+/*
+ * CST8218 Assignment 1
+ * Author: Lucas Estienne <esti0011@algonquinlive.com>
+ * 2020W Term
+ */
+package cst8218.esti0011.entity;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+
+/** 
+ * Abstract Facade for Sprite beans
+ * @author tgk
+ * @version 1.0
+ * @since 1.0
+ */
+public abstract class AbstractFacade<T> {
+    private Class<T> entityClass;
+
+    public AbstractFacade(Class<T> entityClass) {
+        this.entityClass = entityClass;
+    }
+
+    protected abstract EntityManager getEntityManager();
+
+    public void create(T entity) {
+        getEntityManager().persist(entity);
+    }
+
+    public void edit(T entity) {
+        getEntityManager().merge(entity);
+    }
+
+    public void remove(T entity) {
+        getEntityManager().remove(getEntityManager().merge(entity));
+    }
+
+    public T find(Object id) {
+        return getEntityManager().find(entityClass, id);
+    }
+
+    public List<T> findAll() {
+        javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        cq.select(cq.from(entityClass));
+        return getEntityManager().createQuery(cq).getResultList();
+    }
+
+    public List<T> findRange(int[] range) {
+        javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        cq.select(cq.from(entityClass));
+        javax.persistence.Query q = getEntityManager().createQuery(cq);
+        q.setMaxResults(range[1] - range[0] + 1);
+        q.setFirstResult(range[0]);
+        return q.getResultList();
+    }
+
+    public int count() {
+        javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        javax.persistence.criteria.Root<T> rt = cq.from(entityClass);
+        cq.select(getEntityManager().getCriteriaBuilder().count(rt));
+        javax.persistence.Query q = getEntityManager().createQuery(cq);
+        return ((Long) q.getSingleResult()).intValue();
+    }
+    
+}
---
+++ assignments/assignment-1/SpriteLucas/SpriteLucas-ejb/src/java/cst8218/esti0011/util/ColorValidator.java
@@ -0,0 +1,60 @@
+/*
+ * CST8218 Assignment 1
+ * Author: Lucas Estienne <esti0011@algonquinlive.com>
+ * 2020W Term
+ */
+package cst8218.esti0011.util;
+
+
+import java.awt.Color;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+/**
+ * Custom validator for our JSF Color inputs.
+ * Validates that input strings match the regex for HEX color codes, minus the pound sign (#).
+ * @author lucas.estienne
+ * @version 1.0
+ * @since 1.0
+ * @see java.awt.Color
+ */
+@FacesValidator("cst8218.esti0011.util.ColorValidator")
+public class ColorValidator implements Validator {
+    
+    private static final String HEX_COLORCODE_PATTERN = "^[A-Fa-f0-9]{6}|[A-Fa-f0-9]{3}$";
+    
+    private Pattern pattern;
+    private Matcher matcher;
+    
+    /** Constructor for ColorValidator.
+     * Assigns the pattern variable with the compiled regex for a hex color code.
+     */
+    public ColorValidator(){
+        pattern = Pattern.compile(HEX_COLORCODE_PATTERN);
+    }
+    
+    /** Perform the correctness checks for the Color input string.
+     * @param context FacesContext for the request being processed
+     * @param component UIComponent we are checking for correctness
+     * @param value the value to validate
+     */
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        Color color = (Color) value;
+        String hs = Integer.toHexString(color.getRGB()).substring(2);
+        matcher = pattern.matcher(hs);
+        if(!matcher.matches()){
+            FacesMessage msg = new FacesMessage("Color validation failed.", "Must be a valid Hex color code (pound sign ommitted, i.e. 'FFFFFF').");
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(msg);
+        }
+
+    }
+}
